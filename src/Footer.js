b8ee9ad5f55fa369bc926e4c516db4ee55660211
Nyi Htet <nyihtet4dev@icloud.com>
2023-06-29 00:29:58 -0700

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return(
+        <div className = "footer">
+            This is for the social media links
+        </div>
+    );
+}
+
+export default Footer;
