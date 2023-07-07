e15eae90a87106ddcd686ee1f9c40ec5294df07a
Nyi Htet <nyihtet4dev@icloud.com>
2023-07-07 12:47:52 -0700

    day 9

---
+++ src/About.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+
+function About() {
+  return (
+    <div className="about">
+      <h2>Educational Background</h2>
+      <div className="timeline">
+        <div className="timeline-item">
+          <h3>2019 - Present</h3>
+          <h4>Ohlone College</h4>
+          <p>Bachelor of Science in Computer Science</p>
+          <p>Major GPA: 3.8</p>
+        </div>
+        <div className="timeline-item">
+          <h3>2017 - 2019</h3>
+          <h4>High School Name</h4>
+          <p>High School Diploma</p>
+          <p>Achievements: Honor Roll</p>
+        </div>
+        <div className="timeline-item">
+          <h3>2014 - 2017</h3>
+          <h4>Middle School Name</h4>
+          <p>Achievements: Student Council Member</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
 import './App.css';
 import './Logo.png';
-import Rate from './Rate.js';
-import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 
+import * as Three from 'three';
+import gifImage from './among-us.gif';
+import Rate from './Rate.js';
+import About from './About.js';
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import React, { useState } from 'react';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  function toggleTheme() {
+    setTheme(prevTheme => ( prevTheme === 'light' ? 'dark' : 'light'));
+  }
   function Home(){
-    return <h2> Home </h2>;
+    return (
+      <div>
+        <div className = "image-container">
+       <img src={require('./port.jpg')} className = "profile-image" width = "200" />
+       </div>
+
+       <p class = "first-paragraph">
+        "Aspiring software engineering"
+       </p>
+
+       
+      </div>
+    );
+  }
+
+  function Projects(){
+    return (
+      <div>
+        <div className = "weather-app">
+        <a href='https://my-mapify.vercel.app/'>
+          <img  className = "weather-image" width = "300" src={require('./weatherapp.png')} />
+        </a>
+        <p className = "weather-content">
+          A simple react weather application 
+          <br/>
+          Generate the temperature and weather condition of a specific city
+        </p>
+        </div>
+      </div>
+    );
   }
 
   function About(){
-    return <h2> About </h2>;
+    return (
+      <div>
+        <div className="about">
+      <h2>Educational Background</h2>
+      <div className="timeline">
+      <div className="timeline-item">
+          <h3>2022 - Present</h3>
+          <h4>Ohlone College</h4>
+          <p>Bachelor of Science in Computer Science</p>
+          <p>Major GPA: 4.0</p>
+        </div>
+        <div className="timeline-item">
+          <h3>2019 - 2020</h3>
+          <h4>UCSY</h4>
+          <p>Bachelor of Science in Computer Science</p>
+        </div>
+        <div className="timeline-item">
+          <h3>2017 - 2019</h3>
+          <h4>  CAE </h4>
+          <p>High School Diploma</p>
+          <p>Achievements: Honor Roll</p>
+        </div>
+        <div className="timeline-item">
+          <h3>2014 - 2017</h3>
+          <h4>Middle School BEHS(3)</h4>
+          <p>Achievements: Student Council Member</p>
+        </div>
+      </div>
+    </div>
+      </div>
+    );
   }
 
   function Contact(){
-    return <h2> Contact </h2>;
+    return (
+      <div className = "contact"> 
+      <div className = "gifContainer">
+      <img width="200" src={gifImage} alt="Example GIF" />
+      </div>
+      <div className = "contactInfo"> 
+      <p>Phone number: 5103589824</p>
+        <p>Email: nyihtet4dev@icloud.com</p>
+        <p>Address: 39042 donnerway fremont CA</p>
+      </div>
+      </div>
+    );
   }
   return (
   
     
-      <div className="App Full-page">
+      <div className={`App Full-page ${theme}`}>
         <div>
         <Router>
         <nav>
@@ -29,10 +108,18 @@ function App() {
               <Link to="/" className="nav-link"> Home </Link>
             </li>
             <li>
+              <Link to="/projects" className="nav-link"> Projects </Link>
+            </li>
+            <li>
               <Link to="/about" className="nav-link"> About </Link>
             </li>
             <li>
               <Link to="/contact" className="nav-link"> Contact </Link>
+            </li>
+            <li>
+            <button onClick={toggleTheme}>
+         <img src={require('./light.png')} width = "50" height = "50" />
+       </button>
             </li>
           </ul>
           </div>
@@ -40,6 +127,7 @@ function App() {
 
        <Routes>
        <Route path = "/" Component={Home} />
+       <Route path = "/projects" Component = {Projects} />
        <Route path = "/about" Component={About}/>
        <Route path = "/contact" Component={Contact}/>
        </Routes>
@@ -47,16 +135,6 @@ function App() {
        </Router>
         </div>
         
-       <div className = "image-container">
-       <img src={require('./port.jpg')} className = "profile-image" width = "200" />
-       </div>
-       
-
-
-       <p class = "first-paragraph">
-        "Aspiring software engineering"
-       </p>
-
        
 
        
