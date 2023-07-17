309de7a53c3f82ca649a03c1a24a4640a156deac
Nyi Htet <nyihtet4dev@icloud.com>
2023-07-16 20:04:47 -0700

    Day 18

--- src/Certifications.js
+++ src/Certifications.js
@@ -44,6 +44,42 @@ function Certifications() {
             </div>
           </li> 
   </div>
+  <br/><br/><br/>
+  <div class="row" style={{justifyContent:'space-between'}}>
+  
+  <li class = "col-sm-5 card" style={{borderRadius: '30px',background:'white'}}>
+    <div class = "row p-4" style={{margin: "20px", fontFamily:"fantasy"}}>
+    <img class = "col-sm-3" src = { require('./google.png')} />
+    <h3 class = "col-sm-9" style={{display:'flex', alignItems:'center'}}> Foundations of User Experience  </h3>
+    </div>
+            <div style={{fontSize:"20px", marginBottom:'50px'}}>
+              <li>User Experience Design</li>
+              <li> Wireframing</li>
+              <li> Prototyping </li>
+              <li> UX Research</li>
+              <li> User Experience (UX) </li>
+              <a role="button" target="_blank" href="https://www.coursera.org/account/accomplishments/certificate/2D598CL97NHA" 
+              style={{marginTop:'40px'}} class="btn btn-outline-secondary">Show Credential</a>
+            </div>
+            
+   </li>
+   <li class = "col-sm-5 card" style={{borderRadius: '30px',background:'#3b5998'}}>
+    <div class = "row" style={{margin: "20px", fontFamily:"fantasy", color:"white"}}>
+            <img class = "col-sm-3" style={{margin: "20px"}} src = { require('./meta.png')} />
+            <h3 class = "col-sm-7 text-justify" style={{display:'flex', alignItems:'center', margin: "20px"}}> Introduction to IOS development </h3>
+            </div>
+
+            <div style={{fontSize:"20px", padding:"30px", marginBottom:'50px', color:'white'}} class="font-weight-bold">
+              <li> Application Development</li>
+              <li> iOS App Development</li>
+              <li> Mobile Development</li>
+              <a role="button" target="_blank" href="https://www.coursera.org/account/accomplishments/certificate/X47BZXM6HP8B" 
+               style={{marginTop:'60px'}} class="btn btn-primary">Show Credential</a>
+            </div>
+          </li> 
+  </div>
+
+
 </div>
           
 
