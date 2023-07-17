91c6157cf239735ad1a0fbca514e7d5c52711de2
Nyi Htet <nyihtet4dev@icloud.com>
2023-07-17 11:07:18 -0700

    Day 19

--- src/Music.js
+++ src/Music.js
@@ -2,7 +2,15 @@ function Music() {
     return(
         <div>
             <img src={require('./Music.gif')}/>
-            
+            <h2> What is the founder of Apple </h2>
+            <div class="custom-control custom-radio">
+  <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input"/>
+  <label class="custom-control-label" for="customRadio1">Estein</label>
+</div>
+<div class="custom-control custom-radio">
+  <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input"/>
+  <label class="custom-control-label" for="customRadio2">Bill Gate</label>
+</div>
         </div>
     );
 }
