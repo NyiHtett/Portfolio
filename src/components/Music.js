cb93b55ffe8607e636cb1142aeb22f9fc9aa5229
Nyi Htet <nyihtet4dev@icloud.com>
2023-09-17 21:49:49 -0700

    Day 82

--- src/components/Music.js
+++ src/components/Music.js
@@ -2,7 +2,7 @@ function Music() {
     return(
         <div style={{display:"flex", flexDirection:"column", alignItems:"center", gap:100}}>
             <img width="400" src={require('../assets/Music.gif')}/>
-            <audio controls>
+            <audio controls loop>
             <source src={require('../assets/loveLike.mp3')} type="audio/mpeg" />
             </audio>
         </div>
