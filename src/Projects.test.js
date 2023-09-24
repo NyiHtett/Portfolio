3f0b09eb46359e69e1d9fd8e974991172ffa4e8e
Nyi Htet <nyihtet4dev@icloud.com>
2023-09-24 09:37:25 -0700

    Day 89

--- src/Projects.test.js
+++ src/Projects.test.js
@@ -3,7 +3,17 @@ import {screen, render, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Projects from "./components/Projects";
 
-test("images all render successfully", ()=>{
-    const image = screen.getByAltText("Card image cap"); 
-    expect(image).toBeInTheDocument(); 
+test("all project texts render successfully", ()=>{
+    render(<Projects/>)
+    expect(screen.getByText("Mapify")).toBeInTheDocument(); 
+    expect(screen.getByText("Header & Slider")).toBeInTheDocument(); 
+    expect(screen.getByText("Good Work Hub Flutter app")).toBeInTheDocument();
+    expect(screen.getByText("Calculator")).toBeInTheDocument();
+    expect(screen.getByText("Artistic Flowfolio")).toBeInTheDocument();
+    expect(screen.getByText("BookShareHub")).toBeInTheDocument();
+    expect(screen.getByText("AgeVerifyPro")).toBeInTheDocument();
 });
+
+// should open a project description when a project is clicked
+// should navigate to the project website when "Go to the website" is clicked
+// Assert that the window location has changed to the project's website URL
