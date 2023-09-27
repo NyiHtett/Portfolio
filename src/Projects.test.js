c10d240a4ba05b6b043c46304dc592abd6fc26dc
Nyi Htet <nyihtet4dev@icloud.com>
2023-09-26 20:22:08 -0700

    Day 91

--- src/Projects.test.js
+++ src/Projects.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {screen, render, fireEvent} from '@testing-library/react';
+import {screen, render, fireEvent, waitFor} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Projects from "./components/Projects";
 
@@ -14,12 +14,18 @@ test("all project texts render successfully", ()=>{
     expect(screen.getByText("AgeVerifyPro")).toBeInTheDocument();
 });
 
-test("correctly navigate to Mapify website", ()=>{
-    const button = screen.getByTitle('Go to the website', { selector: 'a[href="https://my-mapify.vercel.app/"]'});
-    fireEvent.click(button); 
-    expect(window.location.href).toBe("https://my-mapify.vercel.app/");
-})
-
+test("correctly navigate to Mapify website", async () => {
+    render(<Projects />);
+    const button = screen.getByText('Go to the website', { selector: 'a[href="https://my-mapify.vercel.app/"]' });
+  
+    // Trigger the click event on the button
+    fireEvent.click(button);
+  
+    // Use async/await with waitFor to wait for the navigation to complete
+    await waitFor(() => {
+      expect(window.location.href).toBe("https://my-mapify.vercel.app/");
+    });
+  });
 // should open a project description when a project is clicked
 // should navigate to the project website when "Go to the website" is clicked
 // Assert that the window location has changed to the project's website URL
