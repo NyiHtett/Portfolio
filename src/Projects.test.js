2d3a7559d6e569669484e4f8a48d440b1594b5db
Nyi Htet <nyihtet4dev@icloud.com>
2023-09-28 11:38:01 -0700

    Day 93

--- src/Projects.test.js
+++ src/Projects.test.js
@@ -30,9 +30,32 @@ test("correctly navigate to Header & Slider project", () => {
 
 test("correctly navigate to the github profile", () => {
   render(<Projects/>);
-  const button = screen.getByText('Go to the website', { selector: 'a[href="https://github.com/goodworkhub-dev/flutter-mobile-app"]'});
+  const button = screen.getByText('Go to the git repo', { selector: 'a[href="https://github.com/goodworkhub-dev/flutter-mobile-app"]'});
   fireEvent.click(button);
 });
+
+test("correctly navigate to the calculator profile", () => {
+  render(<Projects/>);
+  const button = screen.getByText('Go to the website', { selector: 'a[href="https://calculator-react-nyihtett.vercel.app/"]'})
+  fireEvent.click(button);
+})
+
+test("correctly navigate to artistic flowfolio", () => {
+  render(<Projects/>);
+  const button = screen.getByText('Go to the website', { selector: 'a[href="https://nyi-practice.webflow.io/"]' });
+})
 // should open a project description when a project is clicked
 // should navigate to the project website when "Go to the website" is clicked
 // Assert that the window location has changed to the project's website URL
+
+test("correctly navigate to book share hub website", () => {
+  render(<Projects/>);
+  const button = screen.getByText('Go to the website', { selector: 'a[href="https://book-share-hub.vercel.app/"]'})
+  fireEvent.click(button);
+})
+
+test("correctly navigate to Age Verify Pro website", () => {
+  render(<Projects/>);
+  const button = screen.getByText('Go to the website', { selector: 'a[href="https://ageverify.vercel.app/"]'});
+  fireEvent.click(button);
+})
