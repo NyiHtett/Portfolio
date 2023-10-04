e8778bb563ceb3699387fab99124a2675dcfd40c
Nyi Htet <nyihtet4dev@icloud.com>
2023-10-03 23:46:03 -0700

    Day 98

--- src/Contact.test.js
+++ src/Contact.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import Contact from "./components/Contact";
 
@@ -18,4 +18,36 @@ describe('Contact component', () => {
         expect(email).toBeInTheDocument(); 
         expect(email).toHaveAttribute('href','mailto:nyihtet4dev@icloud.com');
     })
+
+    // test('Loader is present when isLoading is true', () => {
+    //     // Render the Contact component with isLoading set to true
+    //     //const { getByTestId } = render(<Contact isLoading={true} phNumber="1234567890" />);
+      
+    //     // Use getByTestId to find the loader element by its data-testid attribute
+    //     const loaderElement = screen.queryByTestId('loader');
+      
+    //     // Assert that the loader element is present in the rendered component
+    //     expect(loaderElement).toBeInTheDocument();
+    //   });
+    
+
+    it('renders the address finder button', () =>{
+        render(<Contact/>);
+        const mapButton = screen.getByText("39042 donnerway fremont CA");
+        expect(mapButton).toBeInTheDocument(); 
+        fireEvent.click(mapButton); 
+    })
+
+    it('ending prompt is displayed', () => {
+        render(<Contact/>);
+        const text = screen.getByText("Click on the items to see the functions");
+        expect(text).toBeInTheDocument();
+    })
+
+    it('distance is prompted', ()=> {
+        render(<Contact isLoading = {false}/>);
+        const distance = 3;
+    })
+
+    
 })
