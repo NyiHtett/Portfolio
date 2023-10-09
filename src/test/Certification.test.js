5a68a68061fdfe01d544110e1a2a3c5b828abd70
Nyi Htet <nyihtet4dev@icloud.com>
2023-10-09 11:50:36 -0700

    Day 103

--- src/test/Certification.test.js
+++ src/test/Certification.test.js
@@ -1,20 +1,23 @@
 import React from "react";
 import {screen, render, fireEvent} from '@testing-library/react';
+import { createMemoryRouter } from "react-router-dom";
 import '@testing-library/jest-dom';
 import Certifications from "../components/Certifications";
 
 describe("Certification component", ()=> {
-    it("technical support included", ()=> {
-        render(<Certifications/>);
-        const title1 = screen.getByText("Technical Support Fundamentals");
-        expect(title1).toBeInTheDocument();
-
-        const googleLogo = screen.getByAltText("Google Logo");
-        expect(googleLogo).toBeInTheDocument(); 
-
+    it("should open a specific website when the 'Show Credential' button is clicked", () => {
+        render(<Certifications />);
         const button = screen.getByText("Show Credential");
-        expect(button).toBeInTheDocument();
-    })
+      
+        // Click the button
+        fireEvent.click(button);
+      
+        // Assert that window.open was called with the expected URL
+        expect(window.open).toHaveBeenCalledWith(
+          "https://www.coursera.org/account/accomplishments/certificate/HRFTJUXSQ7RZ",
+          '_blank'
+        );
+      });
 
 
 })
