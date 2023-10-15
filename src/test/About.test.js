2e33cfc1eba10d90f2a7e57dd59f98029a5ce732
Nyi Htet <nyihtet4dev@icloud.com>
2023-10-14 23:48:18 -0700

    none

--- src/test/About.test.js
+++ src/test/About.test.js
@@ -4,10 +4,16 @@ import "@testing-library/jest-dom";
 import About from "../components/About.js";
 
 describe("about page is rendered", ()=> {
-    it("computer science included", async ()=>{
+    it("ohlone computer science included", async ()=>{
         render(<About/>);
         await new Promise(resolve => setTimeout(resolve, 1000));
         const present = screen.getByText("Ohlone College");
         expect(present).toBeInTheDocument();
+    })
+
+    it("ucsy included", () => {
+        render(<About/>);
+        const uni = screen.getByText("UCSY");
+        expect(uni).toBeInTheDocument();
     })
 })
